a9daf29a1f9b5ccfc567ee13a0d943fe3d7b11e6
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-27 21:24:29 +0530

    Added : Finding the word by using BinarySearch Method

---
+++ BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BinarySearch Program");
+        BinarySearch search = new BinarySearch();
+        String[] array = {"apple", "ball", "cat", "dog"};    //  apple ,ball , cat , dog
+        String word = "apple";                               //    0      1     2      3  -->indexes
+        int result = search.searchingMethod(array, word);    //  l=0                   r=3
+        if (result == -1)
+            System.out.println("Element not found in the data");
+        else
+            System.out.println("Element found in the data at Index : " + result);
+    }
+
+    public int searchingMethod(String[] array, String word) {
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (word.compareTo(array[mid]) == 0) {
+                return mid;
+            } else if (word.compareTo(array[mid]) > 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
